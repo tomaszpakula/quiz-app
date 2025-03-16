b0c7f50ad2d0837829fb6da2ebc738da8566ac5d
tomaszpakula <tomasz.pakula11@gmail.com>
2025-03-16 10:56:35 +0100

    updating code

--- src/AlertComponent.js
+++ src/AlertComponent.js
@@ -1,18 +1,16 @@
 import { Alert } from "@mui/material";
 import React from "react";
-import { CSSTransition } from "react-transition-group";
 import { useQuestion } from "./QuestionProvider";
 import "./App.css";
 
 export default function AlertComponent() {
   const { success } = useQuestion();
-  const { alertOn } = useQuestion();
   return (
     <Alert
       severity={success ? "success" : "error"}
       style={{ position: "absolute", width: "10rem", margin: "1rem" }}
     >
-      {success ? "Dobrze" : "Źle"}
+      {success ? "Good!" : "Incorrect!!!"}
     </Alert>
   );
 }
